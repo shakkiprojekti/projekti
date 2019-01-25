d5141592fc82c6664abcd3a2fbb0b1421297d16f
AdolfHutler <34023685+AdolfHutler@users.noreply.github.com>
2019-01-25 08:57:08 +0200

    Create Nappula.cpp

---
+++ Nappula.cpp
@@ -0,0 +1,49 @@
+#include "Nappula.h"
+#include <iostream>
+
+
+Nappula::Nappula(std::wstring unicode, int vari, int koodi) {
+
+	_unicode = unicode;
+	_vari = vari;
+	_koodi = koodi;
+
+}
+
+//Nappula::Nappula() {};
+
+void Nappula::setKoodi(int koodi) {
+
+	_koodi = koodi;
+
+}
+
+int Nappula::getKoodi() {
+
+	return _koodi;
+
+}
+
+void Nappula::setUnicode(std::wstring unicode) {
+
+	_unicode = unicode;
+
+}
+
+std::wstring Nappula::getUnicode() {
+
+	return _unicode;
+
+}
+
+void Nappula::setVari(int vari) {
+
+	_vari = vari;
+
+}
+
+int Nappula::getVari() {
+
+	return _vari;
+
+}
