9c33422893ce9c96ea5889af19959608faf9aa57
AdolfHutler <34023685+AdolfHutler@users.noreply.github.com>
2019-01-25 10:28:42 +0200

    Create Siirto.h

---
+++ Siirto.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Ruutu.h"
+class Siirto {
+public:
+	Siirto(Ruutu, Ruutu);
+	Siirto() {}
+	Siirto(bool lyhytLinna, bool pitkaLinna); // Poikkeussiirto linnoitusta varten
+	Ruutu getAlkuruutu();
+	Ruutu getLoppuruutu();
+	bool onkoLyhytLinna();
+	bool onkoPitkälinna();
+private:
+	Ruutu alkuRuutu;
+	Ruutu loppuRuutu;
+	int miksiKorotetaan = 0;
+	bool lyhytLinna;
+	bool pitkalinna;
+};
