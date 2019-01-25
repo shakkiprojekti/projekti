fb0e5eb668a3385a243a8877c673fec6ff9f7f90
AdolfHutler <34023685+AdolfHutler@users.noreply.github.com>
2019-01-25 10:29:06 +0200

    Create Ruutu.cpp

---
+++ Ruutu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Ruutu.h"
+using namespace std;
+
+Ruutu::Ruutu(int rivi, int sarake)
+{
+
+}
+
+int Ruutu::getRivi()
+{
+	return 0;
+}
+
+int Ruutu::getSarake()
+{
+	return 0;
+}
