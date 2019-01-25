675d82ef5553bace1939f231158cc1c751faf147
AdolfHutler <34023685+AdolfHutler@users.noreply.github.com>
2019-01-25 08:54:35 +0200

    Update Asema.h

--- Asema.h
+++ Asema.h
@@ -1,8 +1,29 @@
 #pragma once
-class Asema {
+#include "Nappula.h"
+
+class Asema : public Nappula {
 
 public:
 	//Konstruktori luo alkuaseman laudalle
 	Asema();
 	Nappula* lauta[8][8];
+	static Nappula* vk;
+	static Nappula* vd;
+	static Nappula* vt;
+	static Nappula* vl;
+	static Nappula* vr;
+	static Nappula* vs;
+
+	static Nappula* mk;
+	static Nappula* md;
+	static Nappula* mt;
+	static Nappula* ml;
+	static Nappula* mr;
+	static Nappula* ms;
+
+private:
+
 };
+
+
+
